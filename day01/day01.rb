bb0adc075275520c5abff35227e7a83bb2602dc1
Luis Carlin <luiscarlin@gmail.com>
2023-10-04 12:09:49 -0400

    part 2 done

--- day01/day01.rb
+++ day01/day01.rb
@@ -1,5 +1,20 @@
-file = File.open('./day01/file.in')
+def part_1
+  file = File.open('./day01/file.in')
 
-max_sum = file.read.split("\n\n").map { |x| x.split("\n").map(&:to_i) }.map(&:sum).max
+  max_sum = file.read.split("\n\n").map { |x| x.split("\n").map(&:to_i) }.map(&:sum).max
 
-puts "Part 1: #{max_sum}"
+  puts "Part 1: #{max_sum}"
+end
+
+def part_2
+  file = File.open('./day01/file.in')
+
+  sum_top_three = file.read.split("\n\n").map do |x|
+    x.split("\n").map(&:to_i)
+  end.map(&:sum).sort.reverse.first(3).reduce(:+)
+
+  puts "Part 2: #{sum_top_three}"
+end
+
+part_1
+part_2
